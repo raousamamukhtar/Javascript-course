9f4abb6548188acb48d49b5ddc9e344ac50e2231
rmusamamukhtar <RMUSAMAMUKHTAR@GMAIL.COM>
2023-04-26 06:59:29 +0500

    javascript

---
+++ chap-6/index.js
@@ -0,0 +1,100 @@
+// function addFive(num){
+//     let result = num + 5;
+//     console.log(result);
+// }
+// addFive(2);
+
+
+
+// function eidMubarak(name){
+//     let result = `Hello ${name},EID MUBARAK!`
+//     console.log(result);
+// }
+// eidMubarak("Hamza");
+
+
+// function sumOfNumbers(num1,num2){
+//         if(num1>num2){
+//             let result = num1+10;
+//             console.log(result);
+//         }else {
+//             let result = num2-2;
+//             console.log(result);
+//         }
+//     }
+//     sumOfNumbers(3,12);
+
+
+
+// let arr1 = [1,2,3];
+// let arr2 = [4,5,6];
+
+// let arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+
+// // EX 6.1
+// function sumOfNumbers(num1,num2){
+//     let result = num1 + num2
+//     console.log(result)
+// }
+// sumOfNumbers(12,3);
+// sumOfNumbers(32,2);
+
+
+
+//Ex 6.2
+// const words = [ "zero","one","two","three"];
+// const randomNum = Math.floor(Math.random()*4);
+
+// let result = words[randomNum];
+
+// function userInput(){
+//     let you = prompt("Enter Your Name here plz : ");
+//     document.write(`Hello ${you} plz write your name here `)
+// }
+
+// userInput();
+
+
+function tester(para1, para2){
+    return para1 + " " + para2;
+   }
+   const arg1 = "argument 1";
+   const arg2 = "argument 2";
+   tester(arg1, arg2);
+
+
+
+
+
+
+
+// function userName(){
+// let you = prompt("Enter your name here ");
+// console.log(`Hello My son ${you} you are a bloodyshit`);
+// }
+
+// userName();
+
+// function sayHello() {
+//     let you = prompt("What's your name? ");
+//     console.log("Hello", you + "!");
+//    }
+//    sayHello();   
+
+
+
+
+// let varContainingFunction = function() {
+//     let varInFunction = "I'm in a function.";
+//     console.log("hi there!", varInFunction);
+//    };
+//    varContainingFunction();
+
+
+// function sayHello() {
+//     let you = prompt("What's your name? ");
+//     console.log("Hello", you + "!");
+//    }
+//    sayHello();
